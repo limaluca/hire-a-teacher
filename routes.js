ef79949af4b7f112c62de8fecd4ffb6b142911d5
limaluca <lucaslimamesmo@gmail.com>
2020-12-17 19:53:18 -0300

    Teacher creation page added

--- routes.js
+++ routes.js
@@ -9,7 +9,13 @@ routes.get("/teachers", function(request, response) {
     return response.render("teachers/index")
 })
 
-routes.get("/css", function(request, response) {
-    return response.render("teachers/css")
+routes.get("/teachers/create", function(request, response) {
+    return response.render("teachers/create")
 })
+
+routes.post("/teachers", function(request, response) {
+    return response.send("Recebido")
+})
+
+
 module.exports = routes;
