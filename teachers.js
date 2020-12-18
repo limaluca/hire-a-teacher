f656e7f901a2384b53043719ac6374b8a7284f8f
limaluca <lucaslimamesmo@gmail.com>
2020-12-18 16:17:22 -0300

    Writing the data in a JSON

--- teachers.js
+++ teachers.js
@@ -1,5 +1,8 @@
 //Exportando funcoes para POST, CREATE, DELETE
 
+const fs = require('fs');
+const data = require('./data.json')
+
 // POST
 exports.post = function(request, response) {
 
@@ -13,5 +16,14 @@ exports.post = function(request, response) {
         }
     }
 
-    return response.send(request.body);
+    data.teachers.push(request.body)
+
+    // os args de writeFile: Local, coloca-se o data para o formato JSON(null so pra pular e 2 eh o espaçamento no data.json)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+            if (err) return response.send("Write file error!")
+
+            return response.redirect("/teachers")
+        })
+        // return response.send(request.body);
 }
